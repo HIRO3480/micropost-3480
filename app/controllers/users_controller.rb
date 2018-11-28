ddd43f10417c36a9f53f302fd74d4590df30f6fa
hiro.tanaka <ta.hi.rom.14@gmail.com>
2018-11-28 10:41:06 +0000

    like

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   def index
     @users = User.all.page(params[:page])
   end
@@ -28,13 +28,19 @@ class UsersController < ApplicationController
   
   def followings
     @user = User.find(params[:id])
-    @followings = @user.followers.page(params[:page])
+    @followings = @user.followings.page(params[:page])
     counts(@user)
   end
   
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @likes = @user.likings.page(params[:page])
     counts(@user)
   end
 end
